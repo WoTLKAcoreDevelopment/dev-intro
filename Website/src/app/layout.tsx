312d0758885d325fb0ae0643def95e838cb28418
AidanAli <aidanali65@gmail.com>
2024-11-10 01:36:25 -0500

    Fix tailwind css layouts

--- Website/src/app/layout.tsx
+++ Website/src/app/layout.tsx
@@ -2,43 +2,43 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/navbar";
-import Hero from "@/app/components/hero";
-import GameList from "@/app/components/GameList";
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Draenor",
-  description: "Draenor Project",
-
+    title: "Draenor Project",
+    description: "Experience the world of Draenor",
+    icons: {
+        icon: [
+            { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+            { url: '/favicon.svg', type: 'image/svg+xml' },
+            { url: '/favicon.ico', type: 'image/x-icon' },
+        ],
+        apple: [
+            { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+        ],
+    },
 };
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-      <html lang="en" className="flex flex-col min-h-screen">
-      <head>
-          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
-          <link rel="shortcut icon" href="/favicon.ico"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-          <link rel="manifest" href="/site.webmanifest"/>
-          <title>Drenor Project</title>
-          <meta name="description" content="Draenor Project"/>
-      </head>
-      <body className={`${inter.className} flex flex-col min-h-screen bg-gray-900 text-white`}>
-      <Navbar/>
-      <main className="flex flex-col container">
-          {children}
-      </main>
-      <footer className="bg-gray-800 py-4 text-center">
-          <p>&copy; 2024 Draenor Project. All rights reserved.</p>
-      </footer>
-      </body>
-      </html>
-  );
+    return (
+        <html lang="en" className="h-full">
+        <body className={`${inter.className} flex flex-col min-h-screen bg-gray-900 text-white`}>
+        <Navbar />
+        <main className="flex-grow flex-col justify-center   py-15">
+            {children}
+        </main>
+        <footer className=" py-6 mt-auto">
+            <div className="container mx-auto px-4 text-center">
+                <p className="text-gray-300">&copy; {new Date().getFullYear()} Draenor Project. All rights reserved.</p>
+            </div>
+        </footer>
+        </body>
+        </html>
+    );
 }
